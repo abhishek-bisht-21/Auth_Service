44fcc2b3bb3ffdc0a491ec65f54a94a0348e97b2
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2022-12-24 23:38:40 +0530

    Added signup api

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig');
+const apiRoutes = require('./routes/index');
+const { urlencoded } = require('body-parser');
+
+const app = express();
 
 const prepareAndStartServer = () => {
-	app.listen(PORT, () => {
+
+	app.use(bodyParser.json());
+	app.use(bodyParser.urlencoded({extended:true}));
+
+	app.use('/api',apiRoutes);
+
+	app.listen(PORT, async () => {
 		console.log("Server started at port", PORT);
 	})
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
-const router = require('router');
 const apiV1Routes = require('./v1/index');
+
+const router = express.Router();
 
 
 router.use('/v1',apiV1Routes);
+
+module.exports = router;
