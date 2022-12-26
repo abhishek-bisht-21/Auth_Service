1b049371a48cb0cfbb4a05443f777ba80a147e5f
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2022-12-26 11:24:52 +0530

    Added Function to compare password

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 const UserRepository = require('../repository/user-repository');
 const { JWT_KEY } = require('../config/serverConfig');
@@ -20,7 +21,7 @@ class UserService {
 
 	createToken(user) {
 		try {
-			const result = jwt.sign(user, JWT_KEY, {expiresIn: '1d'});
+			const result = jwt.sign(user, JWT_KEY, { expiresIn: '1d' });
 			return result;
 		} catch (error) {
 			console.log("Something went wrong in the token creation");
@@ -28,12 +29,21 @@ class UserService {
 		}
 	}
 
-	verifyToken(token){
+	verifyToken(token) {
 		try {
-			const response = jwt.verify(token,JWT_KEY);
-			return response;	
+			const response = jwt.verify(token, JWT_KEY);
+			return response;
 		} catch (error) {
 			console.log("Something went wrong in token validation", error);
+			throw error;
+		}
+	}
+
+	checkPassword(userInputPlainPassword, encryptedPassword) {
+		try {
+			return bcrypt.compareSync(userInputPlainPassword, encryptedPassword);
+		} catch (error) {
+			console.log("Something went wrong in password comparison");
 			throw error;
 		}
 	}
